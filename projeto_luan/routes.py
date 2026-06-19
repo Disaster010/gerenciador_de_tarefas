daa247b606c5e2537b714314ede00ead60e63710
Disaster010 <luangabrielf6@gmail.com>
2026-06-19 16:12:28 -0300

    Projeto_luan

--- projeto_luan/routes.py
+++ projeto_luan/routes.py
@@ -1,6 +1,6 @@
 from projeto_luan import app, bcrypt, database
 from flask import render_template, url_for, redirect
-from flask_login import login_required, login_user,logout_user, current_user
+from flask_login import login_required, login_user, logout_user, current_user
 from projeto_luan.form import FormLogin, FormCriarConta
 from projeto_luan.models import Usuario
 
@@ -12,8 +12,15 @@ def homepage():
         usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
         if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
             login_user(usuario)
-            return redirect(url_for('perfil', usuario=usuario.username))
+            return redirect(url_for('perfil', id_usuario=usuario.id))
     return render_template('homepage.html', form=formlogin)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
 
 
 @app.route('/criarconta', methods=['GET', 'POST'])
@@ -29,19 +36,19 @@ def criarconta():
         database.session.add(usuario)
         database.session.commit()
         login_user(usuario, remember=True)
-        return redirect(url_for('perfil', usuario=usuario.username))
+        return redirect(url_for('perfil', id_usuario=usuario.id))
 
     return render_template('criarconta.html', form=formcriarconta)
 
 
-@app.route('/perfil/<usuario>')
+@app.route('/perfil/<id_usuario>')
 @login_required
-def perfil(usuario):
-    return render_template('perfil.html', usuario=usuario)
+def perfil(id_usuario):
+    if int(id_usuario) == (current_user.id):
+        # o usuário está vendo o perfil dele
+        return render_template("perfil.html", usuario=current_user)
+    else:
+        # Está vendo perfil de outra pessoa
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template('perfil.html', usuario=usuario)
 
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('homepage'))
